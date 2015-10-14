abee54e824c30f583b80c8118e124c25c5ebb69d
Adam McKerlie <adammckerlie@gmail.com>
2015-10-13 20:23:47 -0400

    Initial Rockstar Button code

---
+++ rockstar-button.ino
@@ -0,0 +1,79 @@
+/*
+ * Rockstar Button
+ * Description: The code that runs the Rockstar Button at G Adventures
+ * Author(s): Adam McKerlie
+ * Version: 0.1
+ * 
+ */
+
+#include <Bounce.h>
+
+// Output Constants
+const int buttonPin = 2;
+const int ledPin = 5;
+
+// Button Declaration
+Bounce rockstarButton = Bounce(buttonPin, 10); // 10 ms debounce
+
+int buttonState = 0;
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+// Function that plays the next song.
+int playNext() {
+  // Make sure we're not sending any modifier keys (ctrl/cmd/shift/etc...)
+  Keyboard.set_modifier(0);
+
+  // Skip to the next song
+  Keyboard.set_key1(KEY_RIGHT);
+  Keyboard.send_now(); 
+
+  // Play music
+  Keyboard.set_key1(KEY_SPACE);
+  Keyboard.send_now();
+
+  // Turn on LED
+  digitalWrite(ledPin, HIGH);
+
+  return 1;
+}
+
+// Function that stops the music
+int stopMusic() {
+  // Make sure we're not sending any modifier keys (ctrl/cmd/shift/etc...)
+  Keyboard.set_modifier(0);
+
+  // Pause the song
+  Keyboard.set_key1(KEY_SPACE);
+  Keyboard.send_now(); 
+
+  // Turn off LED
+  digitalWrite(ledPin, LOW);
+
+  return 0;
+}
+
+void loop() {
+  // Update the status of the button
+  rockstarButton.update();
+
+  // If the button has been pressed do some code
+  if (rockstarButton.risingEdge()) {
+    if (buttonState == 0) {
+      buttonState = playNext();
+    } else {
+      buttonState = stopMusic();
+    }
+  } else {
+    // Make sure we're not sending any modifier keys (ctrl/cmd/shift/etc...)
+    Keyboard.set_modifier(0); 
+    // Sending key1 as 0 releases it
+    Keyboard.set_key1(0);
+    Keyboard.send_now();
+  }
+}
+
